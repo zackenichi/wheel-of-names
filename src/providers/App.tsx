0f43284bfb149abe7e37a4a5a48e003526638334
ZacKenichi <>
2024-08-13 13:15:48 +0800

    input-logic

--- src/providers/App.tsx
+++ src/providers/App.tsx
@@ -1,17 +1,13 @@
 import { FC } from 'react';
 import { PropsWithChildren } from '../resources/PropsWithChildren';
 import { ThemeProvider } from './Theme';
-// import { BrowserRouter } from 'react-router-dom';
-
-// import { Provider } from 'react-redux';
-// import { store } from '../app/store';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
-    // <Provider store={store}>
-    //   <BrowserRouter>
-    <ThemeProvider>{children}</ThemeProvider>
-    //   </BrowserRouter>
-    // </Provider>
+    <Provider store={store}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </Provider>
   );
 };
